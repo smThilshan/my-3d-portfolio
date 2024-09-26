98dc6f9cef998a335f42e43af4a4014729e9eafb
Thilshan Mohamed <smthilshan.com>
2024-09-26 13:03:55 +0530

    initial commit

---
+++ src/hoc/SectionWrapper.jsx
@@ -0,0 +1,19 @@
+import { div } from "framer-motion/client";
+import React from "react";
+import { styles } from "../style";
+import { motion } from "framer-motion";
+import { staggerContainer } from "../utils/motion";
+
+const SectionWrapper = (Component, IdName) => function HOC() {
+
+    return (
+        <motion.section variants={staggerContainer() }  initial='hidden' whileInView='show' viewport={{once: true, amount: 0.25}} className={`${styles.padding} max-w-7xl mx-auto relative z-0`}> 
+        <span className="hash-span" id={IdName}>
+            &nbsp;
+        </span>
+            <Component/>
+        </motion.section>
+    )
+}
+
+export default SectionWrapper
