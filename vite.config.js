70494c1055ee32457c5a9c7a50dc4d22f961e956
Thilshan Mohamed <smthilshan.com>
2024-10-01 22:53:49 +0530

    Build and deploy to gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/my-3d-portfolio/',  // Set this to your repository name on GitHub
   plugins: [react()],
 })
